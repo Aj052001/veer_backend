73b6dc739713c601a7f8e3e9a437f06ecb38d312
Aj052001 <ajsingh052001@gmail.com>
2025-12-15 11:55:23 +0530

    update git.js

---
+++ demo/git.js
@@ -0,0 +1,26 @@
+// git init
+
+// git add .
+
+
+// git commit -m"first commit"
+
+// git branch -M main
+
+// git remote add origin git@github.com:Aj052001/veer_backend.git
+
+// git push -u origin main
+
+
+
+
+// #########################################################################
+
+
+
+// git add .
+
+
+// git commit -m"update git.js"
+
+// git puhs origin main
